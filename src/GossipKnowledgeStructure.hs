77ee37656460635acdefc4f9712e83b7992013bb
JesperK123456 <47450353+JesperK123456@users.noreply.github.com>
2021-06-03 20:26:48 +0200

    worked on GKS

--- src/GossipKnowledgeStructure.hs
+++ src/GossipKnowledgeStructure.hs
@@ -1,26 +1,28 @@
 module GossipKnowledgeStructure
 
 where
+import Data.Graph.Inductive.Graph
 import Data.Map (Map)
 import Data.Set (Set)
-import qualified Data.Graph.Inductive.Graph as Graph
+import Data.List
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 
 import GossipGraph ( GossipGraph )
 
-type Agent = Graph.LNode Char
+-- Todo: weghalen als in GossipGraph staat
+type Agent = LNode Char
 
 data GossipAtom
     = Top                           -- ^ Always true
     | N Agent Agent                 -- ^ Agent x knows the number of agent y
     | S Agent Agent                 -- ^ Agent x knows the secret of agent y
     | C Agent Agent                 -- ^ Agent x has called agent y
-    | Neg GossipAtom                -- ^ Negation
     deriving (Show, Ord, Eq) -- TODO derive Eq?
 
 data GossipForm
     = Atom GossipAtom               -- ^ Atom
+    | Neg GossipAtom                -- ^ Negation
     | Conj [GossipForm]             -- ^ Conjunction
     | Disj [GossipForm]             -- ^ Disjunction   
     | Impl GossipForm GossipForm    -- ^ Implication
@@ -29,7 +31,7 @@ data GossipForm
 data GossipKnowledgeStructure = GKS
   { -- | The set of atoms available in the model
     vocabulary :: Set GossipAtom,
-    
+
     -- | A boolean formula representing the law that every state needs to adhere to
     stateLaw :: GossipForm,
 
@@ -38,7 +40,15 @@ data GossipKnowledgeStructure = GKS
   }
 
 fromGossipGraph :: GossipGraph -> GossipKnowledgeStructure
-fromGossipGraph graph = 
-  let 
-    agents = map snd $ Graph.labNodes graph
+fromGossipGraph graph =
+  let
+    agents = labNodes graph
+
+    -- vocabulary
+    combinations = (. subsequences) . filter . (. length) . (==)
+    agentCombs = combinations 2 agents
+    vocab = Top : foldr (\[x,y] -> (++) [N x y, S x y, C x y]) [] agentCombs
+
+    -- 
+
   in undefined
