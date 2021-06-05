f892bd954e575572bce6c39e6fab0cc4536175aa
Leander-van-Boven <ikhoudvanberichten@gmail.com>
2021-06-05 18:37:17 +0200

    added docstrings, some more nicer functions (please dont judge my haskell too much :) )

--- src/GossipProtocol.hs
+++ src/GossipProtocol.hs
@@ -2,26 +2,38 @@ module GossipProtocol where
 
 import GossipGraph
 
--- | Placeholder
+-- | Placeholder (definitely needs updating!)
 type GossipProtocol = Int
 
--- | Placeholder, possibly something similar (from, to, what)
+-- | (from, to, what), Placeholder, possibly something similar in the end.
 type Action = (Agent, Agent, Relation)
 
+-- | Perform action according to protocol:
+    -- Determine what actions are accepted
+    -- -> Choose the most appropriate action
+    -- -> Execute action against the graph
+    -- -> Return updated graph.
 performProtocolTick :: GossipProtocol -> GossipGraph -> GossipGraph
 performProtocolTick prot graph =
-    -- Perform action according to protocol
-        -- Determine what actions are accepted
-        -- Choose the mose appropriate action
-        -- Execute action against the graph
-        -- Return updated graph
+    -- Needs implementation
     graph
 
+-- | Determines based on the current GossipGraph state which calls are actually allowed to be made.
 acceptedActions :: GossipGraph -> [Action]
 acceptedActions graph = []
 
+-- | Chooses the 'best' action using the rules stated in the protocol and the allowed actions.
+    -- This might also be no action, hence Maybe.
 preferredAction :: GossipProtocol -> [Action] -> Maybe Action
-preferredAction prot [] = Nothing
 preferredAction prot actions =
-    -- To be implemented
-    Just $ head actions
+    case actions of
+        [] -> Nothing 
+        actions -> Just $ head actions  -- To be implemented
+
+-- | Intermediate function (might be merged with performProtocolTick later).
+    -- Performs a graph state change if Just Action, otherwise returns the graph unchanged.
+performAction :: GossipGraph -> Maybe Action -> GossipGraph 
+performAction graph action = 
+    case action of
+        Nothing -> graph
+        Just action -> graph    -- To be implemented
--- src/Program.hs
+++ src/Program.hs
@@ -8,44 +8,80 @@ where
 import GossipGraph
 import GossipProtocol
 
+-- | Main entry point to the program.
+-- Parses the gossipgraph and allows user to select what 
 runProgram :: IO ()
 runProgram = do
   -- Parse Gossip graph
   -- during testing, uncomment line below:
   let graph = testGraph 
   putStrLn "Gossip graph parsed"
-  putStrLn "Useractions (u) or Protocol (p)?"
+  putStrLn "Useractions (u), Protocol (p) or Hybrid (h)?"
   action <- getLine 
   runActions graph action 
   where 
     runActions graph action | action == "u" = userActions graph
                             | action == "p" = protocolActions 1 graph
+                            | action == "h" = hybridActions 1 graph
 
+-- | Combines both user and protocol actions into one function.
+-- Each tick the user may choose to perform a custom action, protocol action or first custom and then protocol action.
+hybridActions :: GossipProtocol -> GossipGraph -> IO ()
+hybridActions prot graph = do
+  putStrLn "Actiontype? (u)ser, (p)rotocol or (b)oth"
+  action <- getLine 
+  newGraph <- executeAction action
+  hybridActions prot newGraph
+  where 
+    executeAction a=
+      case a of 
+        "u" -> performUserAction graph
+        "p" -> performProtocolAction prot graph
+        "b" -> do
+          newGraph <- performUserAction graph
+          performProtocolAction prot newGraph
+  
+-- | Continuous execution of user-action.
 userActions :: GossipGraph -> IO ()
-userActions graph = do
-  putStrLn "What action would you like to perform?"
-  action <- getLine
-  putStrLn ("You want to " ++ action ++ "!?")
-  let graph = performUserAction graph action
-  putStrLn "Updated graph:"
-  _ <- printGraph graph
-  userActions graph      -- Recursive call
-  where
-    performUserAction :: GossipGraph -> String -> GossipGraph
-    performUserAction g a =
-      -- Execute action against GossipGraph
-        -- Update GossipGraph
-        -- Present user with new state (i.e. valuation of observables, current knowledge)
-        -- Display implications of the performed action (i.e. what would be rational actions by the agents)    
-      g
+userActions graph = do 
+  newGraph <- performUserAction graph
+  userActions newGraph  -- Recursive call
 
+-- | Execute action against GossipGraph:
+  -- Update GossipGraph
+  -- -> Present user with new state (i.e. valuation of observables, current knowledge)
+  -- -> Display implications of the performed action (i.e. what would be rational actions by the agents).
+performUserAction :: GossipGraph -> IO GossipGraph 
+performUserAction graph =
+  executeUserAction graph obtainUserAction
+  where
+    obtainUserAction :: IO String
+    obtainUserAction = do
+      putStrLn "What action would you like to perform?"
+      action <- getLine
+      putStrLn ("You want to " ++ action ++ "!?")
+      return action
+
+    executeUserAction :: GossipGraph -> IO String -> IO GossipGraph
+    executeUserAction g a = do
+      action <- a
+      let newGraph = g  -- Do something with action
+      return newGraph
+
+-- | Continuous execution of protocol-actions.
 protocolActions :: GossipProtocol -> GossipGraph -> IO ()
 protocolActions prot graph = do
-  -- Perform protocol tick
-    -- Explain the to be performed actions
-    -- performProtocolTick
-    -- Present user with new state (i.e. valuation of observables, current knowledge)
+  newGraph <- performProtocolAction prot graph
+  protocolActions prot newGraph  -- Recursive call
+
+-- | Perform protocol tick:
+  -- Explain the to be performed actions
+  -- -> performProtocolTick
+  -- -> Present user with new state (i.e. valuation of observables, current knowledge).
+performProtocolAction :: GossipProtocol -> GossipGraph -> IO GossipGraph 
+performProtocolAction prot graph = do
   let graph = performProtocolTick prot graph
-  putStrLn "updateed graph:"
-  _ <- printGraph graph
-  protocolActions prot graph  -- Recursive call
+  putStrLn "Updated graph:"
+  printGraph graph
+  return graph
+      
