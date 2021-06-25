353d1eac2336f885443458dffb8a983cf434cf11
jesper-sk <jesper.kuiper99@gmail.com>
2021-06-25 17:39:42 +0200

    fixed printing of protocol

--- src/GossipProtocol.hs
+++ src/GossipProtocol.hs
@@ -37,7 +37,7 @@ type GossipProtocol = Int -> GossipKnowledgeStructure -> Call -> Form
 
 -- | Converts a protocol to a string notation, as used in (van Ditmarsch et al., 2017). 
 showProtocol :: State -> GossipProtocol -> String
-showProtocol (State g k s) prot = "φ(x,y) = " ++ show (prot (nag k) k (agentFromLab 'x',  agentFromLab 'y')) ++ " ∀x,y"
+showProtocol (State g k s) prot = "φ(x,y) = " ++ show (prot 26 k (agentFromLab 'x',  agentFromLab 'y')) ++ " ∀x,y"
 
 -- | The most simple and naive protocol, it allows any call to be made. φ(a,b) = ⊤
 callAny :: GossipProtocol
